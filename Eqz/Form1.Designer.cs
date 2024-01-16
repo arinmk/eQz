aaee32ac08830191277e205f5011fe7c6ae0d331
arinmk <115914553+arinmk@users.noreply.github.com>
2024-01-16 18:18:43 +0400

    Add project files.

---
+++ Eqz/Form1.Designer.cs
@@ -0,0 +1,277 @@
+﻿namespace eQz
+{
+    partial class eQz
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            DevExpress.XtraCharts.XYDiagram xyDiagram1 = new DevExpress.XtraCharts.XYDiagram();
+            DevExpress.XtraCharts.Series series1 = new DevExpress.XtraCharts.Series();
+            DevExpress.XtraCharts.SideBySideBarSeriesView sideBySideBarSeriesView1 = new DevExpress.XtraCharts.SideBySideBarSeriesView();
+            DevExpress.XtraCharts.RectangleGradientFillOptions rectangleGradientFillOptions1 = new DevExpress.XtraCharts.RectangleGradientFillOptions();
+            this.chartControl1 = new DevExpress.XtraCharts.ChartControl();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.marqueeProgressBarControl1 = new DevExpress.XtraEditors.MarqueeProgressBarControl();
+            this.progressBarControl1 = new DevExpress.XtraEditors.ProgressBarControl();
+            this.progressBarControl2 = new DevExpress.XtraEditors.ProgressBarControl();
+            this.progressBarControl3 = new DevExpress.XtraEditors.ProgressBarControl();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(sideBySideBarSeriesView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.marqueeProgressBarControl1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl2.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl3.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chartControl1
+            // 
+            this.chartControl1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.chartControl1.BorderOptions.Visibility = DevExpress.Utils.DefaultBoolean.False;
+            this.chartControl1.CrosshairEnabled = DevExpress.Utils.DefaultBoolean.False;
+            xyDiagram1.AxisX.ScaleBreakOptions.Style = DevExpress.XtraCharts.ScaleBreakStyle.Straight;
+            xyDiagram1.AxisX.StickToEdge = true;
+            xyDiagram1.AxisX.Visibility = DevExpress.Utils.DefaultBoolean.True;
+            xyDiagram1.AxisX.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisX.VisualRange.Auto = false;
+            xyDiagram1.AxisX.VisualRange.AutoSideMargins = false;
+            xyDiagram1.AxisX.VisualRange.EndSideMargin = 0D;
+            xyDiagram1.AxisX.VisualRange.MaxValueSerializable = "98";
+            xyDiagram1.AxisX.VisualRange.MinValueSerializable = "1";
+            xyDiagram1.AxisX.VisualRange.StartSideMargin = 0D;
+            xyDiagram1.AxisX.WholeRange.Auto = false;
+            xyDiagram1.AxisX.WholeRange.AutoSideMargins = false;
+            xyDiagram1.AxisX.WholeRange.EndSideMargin = 0D;
+            xyDiagram1.AxisX.WholeRange.MaxValueSerializable = "99";
+            xyDiagram1.AxisX.WholeRange.MinValueSerializable = "1";
+            xyDiagram1.AxisX.WholeRange.StartSideMargin = 0D;
+            xyDiagram1.AxisY.Alignment = DevExpress.XtraCharts.AxisAlignment.Far;
+            xyDiagram1.AxisY.Logarithmic = true;
+            xyDiagram1.AxisY.Visibility = DevExpress.Utils.DefaultBoolean.True;
+            xyDiagram1.AxisY.VisibleInPanesSerializable = "-1";
+            xyDiagram1.AxisY.VisualRange.Auto = false;
+            xyDiagram1.AxisY.VisualRange.MaxValueSerializable = "3";
+            xyDiagram1.AxisY.VisualRange.MinValueSerializable = "0.0027";
+            xyDiagram1.AxisY.WholeRange.Auto = false;
+            xyDiagram1.AxisY.WholeRange.MaxValueSerializable = "3";
+            xyDiagram1.AxisY.WholeRange.MinValueSerializable = "0.0001";
+            xyDiagram1.DefaultPane.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.chartControl1.Diagram = xyDiagram1;
+            this.chartControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.chartControl1.IndicatorsPaletteName = "Black and White";
+            this.chartControl1.Legend.LegendID = -1;
+            this.chartControl1.Legend.Visibility = DevExpress.Utils.DefaultBoolean.False;
+            this.chartControl1.Location = new System.Drawing.Point(0, 0);
+            this.chartControl1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.chartControl1.Name = "chartControl1";
+            this.chartControl1.PaletteName = "Violet II";
+            series1.Name = "Series 1";
+            series1.SeriesID = 0;
+            sideBySideBarSeriesView1.BarWidth = 0.5D;
+            sideBySideBarSeriesView1.Color = System.Drawing.Color.FromArgb(((int)(((byte)(112)))), ((int)(((byte)(48)))), ((int)(((byte)(160)))));
+            sideBySideBarSeriesView1.FillStyle.FillMode = DevExpress.XtraCharts.FillMode.Gradient;
+            rectangleGradientFillOptions1.Color2 = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(176)))), ((int)(((byte)(80)))));
+            sideBySideBarSeriesView1.FillStyle.Options = rectangleGradientFillOptions1;
+            series1.View = sideBySideBarSeriesView1;
+            this.chartControl1.SeriesSerializable = new DevExpress.XtraCharts.Series[] {
+        series1};
+            this.chartControl1.Size = new System.Drawing.Size(2201, 718);
+            this.chartControl1.TabIndex = 2;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 5;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.textEdit1.EditValue = "Master display";
+            this.textEdit1.Location = new System.Drawing.Point(0, 678);
+            this.textEdit1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Properties.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(39)))), ((int)(((byte)(39)))), ((int)(((byte)(39)))));
+            this.textEdit1.Properties.Appearance.Font = new System.Drawing.Font("OCR A Extended", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textEdit1.Properties.Appearance.Options.UseBackColor = true;
+            this.textEdit1.Properties.Appearance.Options.UseFont = true;
+            this.textEdit1.Properties.ReadOnly = true;
+            this.textEdit1.Properties.UseReadOnlyAppearance = false;
+            this.textEdit1.Size = new System.Drawing.Size(2201, 40);
+            this.textEdit1.TabIndex = 3;
+            this.textEdit1.EditValueChanged += new System.EventHandler(this.textEdit1_EditValueChanged);
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.simpleButton1.Appearance.Font = new System.Drawing.Font("OCR A Extended", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.simpleButton1.Appearance.ForeColor = System.Drawing.Color.White;
+            this.simpleButton1.Appearance.Options.UseFont = true;
+            this.simpleButton1.Appearance.Options.UseForeColor = true;
+            this.simpleButton1.Location = new System.Drawing.Point(2097, 678);
+            this.simpleButton1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(104, 40);
+            this.simpleButton1.TabIndex = 4;
+            this.simpleButton1.Text = "DEFAULT";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // marqueeProgressBarControl1
+            // 
+            this.marqueeProgressBarControl1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.marqueeProgressBarControl1.EditValue = "Getting Waves";
+            this.marqueeProgressBarControl1.Location = new System.Drawing.Point(0, 672);
+            this.marqueeProgressBarControl1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.marqueeProgressBarControl1.Name = "marqueeProgressBarControl1";
+            this.marqueeProgressBarControl1.Properties.Appearance.BackColor = System.Drawing.Color.Black;
+            this.marqueeProgressBarControl1.Properties.Appearance.Font = new System.Drawing.Font("OCR A Extended", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.marqueeProgressBarControl1.Properties.MarqueeAnimationSpeed = 25;
+            this.marqueeProgressBarControl1.Properties.TextOrientation = DevExpress.Utils.Drawing.TextOrientation.Horizontal;
+            this.marqueeProgressBarControl1.Size = new System.Drawing.Size(2201, 6);
+            this.marqueeProgressBarControl1.TabIndex = 5;
+            // 
+            // progressBarControl1
+            // 
+            this.progressBarControl1.Dock = System.Windows.Forms.DockStyle.Right;
+            this.progressBarControl1.Location = new System.Drawing.Point(2175, 0);
+            this.progressBarControl1.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.progressBarControl1.Name = "progressBarControl1";
+            this.progressBarControl1.Properties.LookAndFeel.SkinMaskColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.progressBarControl1.Properties.LookAndFeel.SkinMaskColor2 = System.Drawing.Color.Lime;
+            this.progressBarControl1.Properties.LookAndFeel.UseDefaultLookAndFeel = false;
+            this.progressBarControl1.Properties.ProgressKind = DevExpress.XtraEditors.Controls.ProgressKind.Vertical;
+            this.progressBarControl1.Properties.Step = 1;
+            this.progressBarControl1.Size = new System.Drawing.Size(26, 672);
+            this.progressBarControl1.TabIndex = 6;
+            // 
+            // progressBarControl2
+            // 
+            this.progressBarControl2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.progressBarControl2.Location = new System.Drawing.Point(2166, 0);
+            this.progressBarControl2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.progressBarControl2.Name = "progressBarControl2";
+            this.progressBarControl2.Properties.LookAndFeel.SkinMaskColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.progressBarControl2.Properties.LookAndFeel.SkinMaskColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.progressBarControl2.Properties.LookAndFeel.UseDefaultLookAndFeel = false;
+            this.progressBarControl2.Properties.ProgressKind = DevExpress.XtraEditors.Controls.ProgressKind.Vertical;
+            this.progressBarControl2.Properties.Step = 1;
+            this.progressBarControl2.Size = new System.Drawing.Size(9, 672);
+            this.progressBarControl2.TabIndex = 7;
+            // 
+            // progressBarControl3
+            // 
+            this.progressBarControl3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.progressBarControl3.Location = new System.Drawing.Point(2138, 0);
+            this.progressBarControl3.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.progressBarControl3.Name = "progressBarControl3";
+            this.progressBarControl3.Properties.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.progressBarControl3.Properties.LookAndFeel.SkinMaskColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.progressBarControl3.Properties.LookAndFeel.SkinMaskColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.progressBarControl3.Properties.LookAndFeel.UseDefaultLookAndFeel = false;
+            this.progressBarControl3.Properties.ProgressKind = DevExpress.XtraEditors.Controls.ProgressKind.Vertical;
+            this.progressBarControl3.Properties.Step = 1;
+            this.progressBarControl3.Size = new System.Drawing.Size(28, 672);
+            this.progressBarControl3.TabIndex = 8;
+            // 
+            // timer2
+            // 
+            this.timer2.Enabled = true;
+            this.timer2.Interval = 1;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // textEdit2
+            // 
+            this.textEdit2.EditValue = "OVER";
+            this.textEdit2.Location = new System.Drawing.Point(2138, -6);
+            this.textEdit2.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.textEdit2.Name = "textEdit2";
+            this.textEdit2.Properties.Appearance.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(38)))), ((int)(((byte)(38)))));
+            this.textEdit2.Properties.Appearance.ForeColor = System.Drawing.Color.Red;
+            this.textEdit2.Properties.Appearance.Options.UseBackColor = true;
+            this.textEdit2.Properties.Appearance.Options.UseForeColor = true;
+            this.textEdit2.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.NoBorder;
+            this.textEdit2.Properties.ReadOnly = true;
+            this.textEdit2.Size = new System.Drawing.Size(63, 34);
+            this.textEdit2.TabIndex = 9;
+            // 
+            // eQz
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2201, 718);
+            this.Controls.Add(this.textEdit2);
+            this.Controls.Add(this.progressBarControl3);
+            this.Controls.Add(this.progressBarControl2);
+            this.Controls.Add(this.progressBarControl1);
+            this.Controls.Add(this.marqueeProgressBarControl1);
+            this.Controls.Add(this.simpleButton1);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.chartControl1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.IconOptions.ShowIcon = false;
+            this.IconOptions.SvgImage = global::eQz.Properties.Resources.electronics_volume;
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "eQz";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "eQz";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(xyDiagram1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(sideBySideBarSeriesView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(series1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.marqueeProgressBarControl1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl2.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.progressBarControl3.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevExpress.XtraCharts.ChartControl chartControl1;
+        private System.Windows.Forms.Timer timer1;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.XtraEditors.MarqueeProgressBarControl marqueeProgressBarControl1;
+        public DevExpress.XtraEditors.ProgressBarControl progressBarControl1;
+        public DevExpress.XtraEditors.ProgressBarControl progressBarControl2;
+        public DevExpress.XtraEditors.ProgressBarControl progressBarControl3;
+        private System.Windows.Forms.Timer timer2;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+    }
+}
+
---
+++ Eqz/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using NAudio.Dsp;
+using System.Linq;
+using NAudio.Wave;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using DevExpress.XtraCharts;
+using System.Linq.Expressions;
+using DevExpress.Utils.Extensions;
+
+
+namespace eQz
+{
+    public partial class eQz : DevExpress.XtraEditors.XtraForm
+    {
+        private double bandaV = 0.00001d;
+        private double cuurValue = 1.9d;
+        private int fftLength = 1024; // Make sure this is a power of 2
+        private Thread myThread;
+        private string outputFilePath = "eQzGarbage.wav"; // Consider making this configurable
+
+        private bool up;
+        private IWaveIn waveIn;
+        private WaveFileWriter writer;
+        public bool Default = false;
+        public string Device;
+
+        public float[] frequencyBands;
+        public double gaba = 10;
+
+        public eQz()
+        {
+            InitializeComponent();
+            AudioCapture();
+        }
+
+        private void AudioCapture()
+        {
+            try
+            {
+                waveIn = new WasapiLoopbackCapture();
+
+                waveIn.DataAvailable += OnDataAvailable;
+                waveIn.RecordingStopped += OnRecordingStopped;
+                waveIn.StartRecording();
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show($"Error initializing audio capture: {ex.Message}");
+                Dispose(true);
+            }
+        }
+        private void defaults()
+        {
+
+            if (Default)
+            {
+                simpleButton1.ForeColor = Color.Red;
+                if (myThread == null || !myThread.IsAlive)
+                {
+                    try
+                    {
+                        myThread = new Thread(() =>
+                        {
+
+                            lock (this)
+                            {
+                                while (Default)
+                                {
+                                    Thread.Sleep(10);
+                                    cuurValue = 2.1;
+                                    bandaV = 0.00001d;
+                                    gaba = 70;
+                                }
+                            }
+
+                        })
+                        { IsBackground = false };
+                        myThread.Start();
+                    }
+                    catch { Dispose(true); }
+                }
+            }
+            else
+            {
+                simpleButton1.ForeColor = Color.White;
+
+                if (myThread != null && myThread.IsAlive)
+                {
+                    myThread.Yield();
+                }
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.Text = " eQz [Displaying Algorithmic]";
+            var sview = (SideBySideBarSeriesView)chartControl1.Series.First().SeriesView;
+            chartControl1.Series.Clear();
+            for (int i = 0; i < 100; i++)
+            {
+                Series series = new Series("", ViewType.Bar);
+                sview.BarWidth = 0.8D;
+                series.View = sview;
+                series.Points.Add(new SeriesPoint(i, 0));
+                chartControl1.Series.Add(series);
+                sview.EqualBarWidth = false;
+
+            }
+        }
+        private void OnDataAvailable(object sender, WaveInEventArgs e)
+        {
+            try
+            {
+                if (writer == null)
+                {
+                    writer = new WaveFileWriter(outputFilePath, waveIn.WaveFormat);
+                }
+
+                writer.Write(e.Buffer, 0, e.BytesRecorded);
+
+                frequencyBands = PerformFFT(e.Buffer, e.BytesRecorded);
+
+            }
+            catch (Exception ex)
+            {
+                //MessageBox.Show($"Error processing audio data: {ex.Message}");
+                Dispose(true);
+            }
+        }
+
+
+        private void OnRecordingStopped(object sender, StoppedEventArgs e)
+        {
+            if (writer != null)
+            {
+                writer.Dispose();
+                writer = null;
+            }
+
+            if (waveIn != null)
+            {
+                waveIn.Dispose();
+            }
+        }
+
+
+        private float[] PerformFFT(byte[] buffer, int bytesRecorded)
+        {
+
+            var waveBuffer = new WaveBuffer(buffer);
+            var complexBuffer = new NAudio.Dsp.Complex[fftLength];
+            for (int i = 0; i < fftLength; i++)
+            {
+                if (i < bytesRecorded / 4)
+                {
+                    complexBuffer[i] = new NAudio.Dsp.Complex();
+
+                    complexBuffer[i].X = waveBuffer.FloatBuffer[i];
+                }
+                else
+                    complexBuffer[i] = new NAudio.Dsp.Complex();
+            }
+
+            if (cuurValue >= 3d)
+            {
+                up = false;
+            }
+            else if (cuurValue <= 1.9d)
+            {
+                up = true;
+            }
+
+            if (up)
+            {
+                cuurValue += 0.0005d;
+                bandaV += 0.000001d;
+            }
+            else
+            {
+                cuurValue -= 0.0005d;
+                bandaV -= 0.000001d;
+            }
+            if (cuurValue < 2)
+            {
+                if (up == true)
+                {
+                    gaba += 0.4;
+                }
+                else
+                {
+                    gaba -= 0.1;
+                }
+            }
+            else if (cuurValue >= 2 && cuurValue <= 2.1)
+            {
+                gaba = 90;
+
+
+            }
+            else if (cuurValue >= 2.15 && cuurValue <= 2.25)
+            {
+                gaba = 10;
+
+            }
+            else if (cuurValue >= 2.3)
+            {
+                gaba = 5;
+            }
+
+            FastFourierTransform.FFT(true, (int)Math.Log(fftLength, cuurValue), complexBuffer);
+
+
+            float[] magnitude = new float[fftLength / 2];
+            for (int i = 0; i < magnitude.Length; i++)
+            {
+                magnitude[i] = (float)Math.Sqrt((complexBuffer[i].X * complexBuffer[i].X) +
+                                                (complexBuffer[i].Y * complexBuffer[i].Y));
+            }
+
+            int numBands = 100;
+            var bandWidth = ((float)fftLength / 2) / numBands;
+            float[] bandAverages = new float[numBands];
+
+            for (int band = 0; band < numBands; band++)
+            {
+                float sum = 0;
+                var startFreq = band * bandWidth;
+                var endFreq = (band + 1) * bandWidth;
+
+                for (var i = startFreq; i < endFreq; i++)
+                {
+                    sum += magnitude[(int)i];
+                }
+
+                bandAverages[band] = (float)Math.Max((sum / bandWidth) * gaba, bandaV);
+            }
+
+            return bandAverages;
+
+        }
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            Default = !Default;
+            defaults();
+            if (Default == true)
+            {
+                this.Text = "eQz [Displaying Locked Default]";
+            }
+            else if (Default == false)
+            {
+                this.Text = "eQz [Displaying Shifter Pattern]";
+            }
+
+        }
+        private void textEdit1_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var sex = Math.Round(cuurValue, 3);
+
+            var sex2 = Math.Round(bandaV, 5);
+            UpdateChart();
+            textEdit1.Text = $"Master Visualiser | Buffer Shift Value: {sex:n3} | Band GB Value: {gaba:n1} | Band AV Shift Value: {sex2:n5}";
+        }
+
+        private void UpdateChart()
+        {
+            if (frequencyBands != null)
+            {
+                for (int i = 0; i < frequencyBands.Length; i++)
+                {
+                    chartControl1.Series[i].Points.BeginUpdate();
+                    chartControl1.Series[i].Points[0].Values.SetValue(frequencyBands[i], 0);
+                    chartControl1.Series[i].Points.EndUpdate();
+                  
+                }
+            }
+        }
+        private void updateGauges()
+        {
+            if (frequencyBands != null)
+            {
+                for (int i = 0; i < frequencyBands.Length; i++)
+                {
+                    progressBarControl1.EditValue = (frequencyBands[i] * 20);
+                    progressBarControl3.EditValue = (frequencyBands[i] * (cuurValue * 30));
+
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            updateGauges();
+        }
+    }
+}
